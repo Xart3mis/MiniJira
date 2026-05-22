e049c4259c2c1c967b739fbd4b8f4ab109821c40
Yassin Diab <yassin.diab@student.giu-uni.de>
2026-05-23 00:09:33 +0300

    add frontend build dir to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,6 +29,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined'));
 
